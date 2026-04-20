da318141f35e73f67f5804a07007c33f6af995da
SangJun <107762636+asdasd7722@users.noreply.github.com>
2026-04-20 15:36:32 +0900

    [level 2] Title: 요격 시스템, Time: 479.30 ms, Memory: 177 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] targets) {
+        int answer = 0;
+        Arrays.sort(targets, (a, b) -> a[1] - b[1]);
+            
+        int prev = -1;
+        for(int i = 0; i < targets.length; ++i) {
+            if(targets[i][0] >= prev) {
+                answer++;
+                prev = targets[i][1];
+            }
+        }
+        return answer;
+    }
+}
